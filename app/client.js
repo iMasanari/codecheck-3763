0a17cd8b8b2d94b6a06f1dc841b38e851a696e06
iMasanari <14dd023@gmail.com>
2016-11-10 23:40:10 +0900

    スマホ用に調整

--- app/client.js
+++ app/client.js
@@ -2,29 +2,36 @@
 
 var ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
 
-$(function () {
-    $('form').submit(function () {
+$(function() {
+    var $m = $('#m');
+
+    $m.focus();
+
+    $('form').submit(function() {
         var $this = $(this);
-        var $m = $('#m');
-        
+
         ws.send(JSON.stringify({ text: $m.val() }));
         $m.val('');
+        $m.focus();
 
         return false;
     });
-    ws.onmessage = function (msg) {
+
+    ws.onmessage = function(msg) {
         var resp = JSON.parse(msg.data);
         var message = resp.type === 'bot' ? resp.text : JSON.parse(resp.text).text;
-        
+
         $('#messages').append(
             $('<li>').append(
                 $('<span class="message">').text(message)
             )
         );
     };
-    ws.onerror = function (err) {
+
+    ws.onerror = function(err) {
         console.log("err", err);
     };
+
     ws.onclose = function close() {
         console.log('disconnected');
     };
