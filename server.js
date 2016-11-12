aba9679e7346ca49f642eae19b52517a1b9c194c
iMasanari <14dd023@gmail.com>
2016-11-12 10:22:27 +0900

    oshareコマンドを追加

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict'
 
 var http = require('http');
+var https = require('https');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var token = require('./token.json');
 
 //////////////////////
 // setting http
@@ -34,19 +36,19 @@ wss.on('connection', ws => {
             }));
         });
 
-        var match = message.text.match(/^(?:bot\s|@bot\s|bot:)(\S+)\s?([\s\S]*)$/);
+        var match = message.text.match(/^(?:@?bot\s|bot:)(\S+)\s?([\s\S]*)$/);
 
         if (match && Bot.hasOwnProperty(match[1])) {
             var data = Bot[match[1]].command(match[2], wss)
 
-            if (data != null) {
+            if (data) {
                 wss.clients.forEach(client => {
                     client.send(JSON.stringify(data));
                 })
             }
         }
     });
-    ws.on('close', function () {
+    ws.on('close', () => {
         console.log('websocket connection close');
     });
 });
@@ -69,5 +71,57 @@ var Bot = {
                 v => Bot[v].description || `bot ${v}`
             ).join('\n')
         })
+    },
+    oshare: {
+        description: 'bot oshare {keyword}: Instagramでタグ検索し、オシャレ度を数値化する',
+        command: (arg, wss) => {
+            const option = {
+                host: 'api.instagram.com',
+                path: `/v1/tags/${encodeURIComponent(arg)}?access_token=${token.instagram}`
+            };
+
+            const callback = res => {
+                const json = JSON.parse(res);
+                const text = (json.meta.code === 200) ?
+                    `「${json.data.name}」のオシャレ度は\n${separate(json.data.media_count)}です` :
+                    `${json.meta.error_type}\n${json.meta.error_message}`;
+
+                wss.clients.forEach(client => {
+                    client.send(JSON.stringify({
+                        success: true,
+                        type: 'bot',
+                        text: text
+                    }));
+                });
+            };
+
+            getRequest(option, callback);
+        }
     }
 };
+
+function separate(num) {
+    return num.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+}
+
+function getRequest(url, callback) {
+    const req = https.get(url, res => {
+        if (res.statusCode !== 200) return;
+
+        res.setEncoding('utf8');
+        res.on('data', function (str) {
+            if (callback) callback(str);
+        });
+    });
+
+    req.setTimeout(1000);
+
+    req.on('timeout', () => {
+        console.log('request timed out');
+        req.abort()
+    });
+
+    req.on('error', err => {
+        console.log("Error: " + err.code + ", " + err.message);
+    });
+}
